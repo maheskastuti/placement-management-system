55bf98c8ed93e4aa2ca6162013dff57fe896b7b7
maheskastuti <stutimaheska1608@gmail.com>
2026-06-19 20:13:53 +0530

    Update database connection password

--- placement.py
+++ placement.py
@@ -3,7 +3,7 @@ import mysql.connector
 conn = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="Stuti16@PATNA",
+    password="YOUR_PASSWORD",
     database="placement_management"
 )
 
